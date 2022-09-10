0c61f4778956fe461ec7540affb5c2f0aaa15a7c
Harwer <HarwerPlay@yandex.ru>
2022-09-10 12:16:53 +0700

    #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next';
-import ChangeTheme from '../components/ChangeTheme';
+import type { NextPage } from "next";
+import ChangeTheme from "../components/ChangeTheme";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="container">
-      <h1>Hello, world!</h1>
-      <ChangeTheme />
+    <div>
+      <Header />
     </div>
   );
 };
